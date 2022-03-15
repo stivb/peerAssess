73c21cbb751a1531793e64b314baeeb33314642d
stivb <stivbennett@gmail.com>
2022-03-15 18:59:04 +0000

    file to reformat the canvas csv

--- readCanvasCsv.R
+++ readCanvasCsv.R
@@ -1,6 +1,34 @@
 library(readr)
-df4 <- read_csv("~/research/results/df4.csv")
+library(reshape)
+library(data.table)
+library(tidyr)
+library(dplyr)
+df4 <- read_csv("C:/Users/comqsjb/Documents/research/peerAssess/cvas/cvaspeereval.csv")
+
+names(df4)[9] <-  "Scores1"
+names(df4)[13] <- "Scores2"
+names(df4)[17] <- "Scores3"
+names(df4)[11] <- "Fbk1"
+names(df4)[15] <- "Fbk2"
+names(df4)[19] <- "Fbk3"
+names(df4)[21] <- "Site1"
+names(df4)[23] <- "Site2"
+names(df4)[25] <- "Site3"
+
+df4<-df4 %>% select(-contains('1.0'))
+df4<-df4 %>% select(-(section:attempt)) 
+df4<-df4[,1:(ncol(df4)-3)]
+df4<-df4[,c("name","id","sis_id","Site1","Scores1","Fbk1","Site2","Scores2","Fbk2","Site3","Scores3","Fbk3")]
+
 dfz<-melt(setDT(df4), id = 1L, 
       measure = list(c(4,7,10), c(5,8,11), c(6,9,12)), 
       value.name = c("Site", "Scores", "Feedback"))
-write.csv(dfz,file="~/research/results/reorg.csv")
+dfx<-separate(dfz,col=Scores,into=c("TechAccuracy", "VizDesign", "SiteAmbition"),sep="\\|")
+
+write.csv(dfx,file="reorg.csv")
+
+dfxfbk<-dfx[,c("name","Site","Feedback")]
+groupdFbk <- dfxfbk %>% select(name, Site, Feedback) %>%  group_by(Site) %>%  mutate(all_feedback = paste(Feedback, collapse = "\n\n"))
+groupdFbk<-groupdFbk[,c("Site","all_feedback")]
+groupdFbk<-groupdFbk[!duplicated(groupdFbk),]
+write.csv(groupdFbk,file="fbk4.csv")
