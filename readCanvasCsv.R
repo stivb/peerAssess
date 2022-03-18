0f8e6953f1147baf2be7c9ad25e0c428f3059805
stivb <s.j.bennett@herts.ac.uk>
2022-03-18 16:27:41 +0000

    now generating analysis sheet too

--- readCanvasCsv.R
+++ readCanvasCsv.R
@@ -4,6 +4,18 @@ library(data.table)
 library(tidyr)
 library(dplyr)
 
+############
+
+
+
+
+joinCells <- function(listIput) {
+   l<-unique(listIput)
+   l<-l[!is.na(l) & l != ""]
+   if (length(l)>1) print(l[length(l)]);
+   return (paste(" ",l[length(l)],sep=""))
+}
+
 fileChosen<-file.choose(new = FALSE)
 csvPath <- dirname(fileChosen)
 
@@ -31,6 +43,31 @@ df4<-df4 %>% select(-(section:attempt))
 df4<-df4[,1:(ncol(df4)-3)]
 df4<-df4[,c("name","id","sis_id","Site1","Scores1","Fbk1","Site2","Scores2","Fbk2","Site3","Scores3","Fbk3")]
 
+#################CREATING THE ANALYTICAL SHEET#####################
+
+df5<-df4[,-(2:3)]
+df5 <- df5 %>% select(-contains("Fbk"))
+df6<-melt(setDT(df5), id = 1L, 
+          measure = list(c(2,4,6), c(3,5,7)), 
+          value.name = c("Site", "Scores"))
+
+df7<-spread(df6,key=Site,value=Scores)
+df8<-df7[,-c(2:3)] 
+dfIrr<-aggregate(df8[,-1], list(df8$name), joinCells)
+
+namez<-colnames(dfIrr[,-1])
+for(i in 1:length(namez)) { 
+   pref<-namez[i]
+   dfIrr<-separate(dfIrr,col=namez[i],
+      into=c(paste0("T",pref), paste0("V",pref), paste0("S",pref)),sep="\\,")
+   
+}
+
+write.csv(dfIrr,file=paste0(csvPath,"/dfIrr.csv"))
+
+
+#################CREATING THE MARKING AUDIT#####################
+
 mksAudit<-df4
 mksAudit<-separate(mksAudit,col=Scores1,into=c("TechAccuracy1", "VizDesign1", "SiteAmbition1"),sep="\\,")
 mksAudit<-separate(mksAudit,col=Scores2,into=c("TechAccuracy2", "VizDesign2", "SiteAmbition2"),sep="\\,")
@@ -39,12 +76,16 @@ mksAudit<-separate(mksAudit,col=Scores3,into=c("TechAccuracy3", "VizDesign3", "S
 write.csv(mksAudit,file=paste0(csvPath,"/mksAudit.csv"))
 
 
+######################RE-ORGANIZED#######################
+
 dfz<-melt(setDT(df4), id = 1L, 
       measure = list(c(4,7,10), c(5,8,11), c(6,9,12)), 
       value.name = c("Site", "Scores", "Feedback"))
 dfx<-separate(dfz,col=Scores,into=c("TechAccuracy", "VizDesign", "SiteAmbition"),sep="\\,")
 
 write.csv(dfx,file=paste0(csvPath,"/reorg.csv"))
+
+######################CREATING THE SUMMARY FOR AWARDING#######################
 
 dfxfbk<-dfx[,c("name","Site","Feedback")]
 groupdFbk <- dfxfbk %>% select(name, Site, Feedback) %>%  group_by(Site) %>%  mutate(all_feedback = paste(Feedback, collapse = "\n\n"))
